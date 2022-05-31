13d9a1fd30df3f27efad2461f866b5b975bbddd9
Ruber Hernández-García <54343006+ruberhg@users.noreply.github.com>
2022-05-30 22:29:55 -0400

    Add files via upload

---
+++ scripts/calculadora.py
@@ -0,0 +1,15 @@
+# calculadora.py
+
+# Tipo de operacion: suma/resta/multiplicación
+operacion = "suma"
+
+# Parámetros de la operación
+a = 4
+b = 7
+
+if operacion == "suma":
+    print(a+b)
+elif operacion == "resta":
+    print(a-b)
+elif operacion == "multiplicacion":
+    print(a*b)
