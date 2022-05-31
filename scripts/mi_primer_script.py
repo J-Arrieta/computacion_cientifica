a710d53b5167445353649078c3c84d392f38772b
Ruber Hernández-García <54343006+ruberhg@users.noreply.github.com>
2022-05-30 22:26:55 -0400

    Add files via upload

--- scripts/mi_primer_script.py
+++ scripts/mi_primer_script.py
@@ -1,11 +1,11 @@
 import math
      
-print("Hola gente del curso de AeroPython")
-print("¿Cuántos sois hoy en clase?")
+print("Hola gente del curso de Computación Científica")
+print("¿Cuántos somos hoy en clase?")
 
 number = input()
 number = int(number)
 root = math.sqrt(number)
 
-print("Ufff! eso es un montón! espero que aprendáis mucho")
+print("Ufff! eso es un montón! espero que aprendan mucho")
 print("Por cierto, la raiz de %i es %f" %(number, root))
